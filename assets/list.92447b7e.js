fb3bfaca1144d6eaa6680d1591d6888a139293fc
Ah jung <735878602@qq.com>
2021-07-22 13:40:18 +0800

    同步预览

---
+++ assets/list.92447b7e.js
@@ -0,0 +1,1 @@
+import{A as e}from"./index.8edf795a.js";function t(t){return e.request({url:"/table/list",method:"get",params:t})}export{t as g};
---
+++ assets/monitor.d4f22d06.js
@@ -0,0 +1,1 @@
+import{d as e,o as r,c as n}from"./vendor.ea668100.js";var o=e({components:{},setup:()=>({})});o.render=function(e,o,t,a,s,d){return r(),n("div",null,"监控台")};export default o;
