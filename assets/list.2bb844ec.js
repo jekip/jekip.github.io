06b4bb8773ffe8aca8bca77d9a4e9d9d07f80f94
Ah jung <735878602@qq.com>
2021-07-30 20:03:37 +0800

    文档和预览更新

---
+++ assets/list.2bb844ec.js
@@ -0,0 +1,1 @@
+import{A as t}from"./index.675c56d3.js";function e(e){return t.request({url:"/table/list",method:"get",params:e})}export{e as g};
---
+++ assets/parentLayout.98c93166.js
@@ -0,0 +1,1 @@
+import{R as r,o,c as e}from"./vendor.36b62bbc.js";const t={};t.render=function(t,n){const s=r("router-view");return o(),e(s)};export default t;
