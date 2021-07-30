54045e0052b12d4d27ee5ede2c1b58860ff554b9
Ah jung <735878602@qq.com>
2021-07-30 10:19:26 +0800

    预览更新

--- assets/list.b080058d.js
+++
@@ -1,1 +0,0 @@
-import{A as t}from"./index.90d0249c.js";function e(e){return t.request({url:"/table/list",method:"get",params:e})}export{e as g};
--- assets/monitor.1eabb00d.js
+++
@@ -1,1 +0,0 @@
-import{d as r,o as e,c as n}from"./vendor.06615944.js";var o=r({components:{},setup:()=>({})});o.render=function(r,o,t,s,a,d){return e(),n("div",null,"监控台")};export default o;
